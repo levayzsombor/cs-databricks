60a1fbdb1937ca0307d19c0c920b7f6310e52e9d
Zsombor Levay <levay1990@gmail.com>
2026-07-16 04:03:00 +0000

    devcontainer

---
+++ test.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import TypeVar
+
+@dataclass
+class Test:
+    name: str
+    value: int
+
+    def display(self) -> None:
+        print(self.name, self.value)
+
+class ChildTest(Test):
+    pass
+
+T = TypeVar("T", bound=Test)
+
+def test_this_thing(x: T) -> None:
+    x.display()
