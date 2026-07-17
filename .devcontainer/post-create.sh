ac2bb6014c1ec425e8fdb095b8c18aab9022e1ee
Zsombor Levay <levay1990@gmail.com>
2026-07-17 05:36:28 +0000

    added uv and ty

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,4 +6,4 @@ cd /workspaces/cs-databricks
 python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install -r requirements-dev.txt
--- typings/__builtins__.pyi
+++ typings/__builtins__.pyi
@@ -1,4 +1,7 @@
+from collections.abc import Callable
+
 from databricks.sdk.runtime import *
+from pyspark.sql import DataFrame
 from pyspark.sql.context import SQLContext
 from pyspark.sql.functions import udf as U
 from pyspark.sql.session import SparkSession
@@ -7,9 +10,10 @@ udf = U
 spark: SparkSession
 sc = spark.sparkContext
 sqlContext: SQLContext
-sql = sqlContext.sql
-table = sqlContext.table
-getArgument = dbutils.widgets.getArgument
+sql: Callable[[str], DataFrame]
+table: Callable[[str], DataFrame]
+
+def getArgument(name: str, defaultValue: str | None = None) -> object: ...
 
 def displayHTML(html: str) -> None: ...
 def display(
