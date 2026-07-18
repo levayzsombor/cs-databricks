5a17b6b75fec23641c0d0a3259ef1a5ec95c4087
Zsombor Levay <levay1990@gmail.com>
2026-07-18 07:24:53 +0000

    node eslint prettier added

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,3 +8,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements-dev.txt
+npm i
+
---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+module.exports = [
+    {
+  extends: [
+    "prettier"
+  ]
+},
+  {
+    ignores: [
+      'node_modules/**',
+      '.venv/**',
+      '.databricks/**',
+      '.ruff_cache/**',
+      'notebooks/**/*.ipynb',
+      'notebooks/outputs/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+    },
+    rules: {
+      curly: 'error',
+      eqeqeq: ['error', 'always'],
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-var': 'error',
+      'object-shorthand': 'warn',
+      'prefer-const': 'warn',
+    },
+  },
+  {
+    files: ['**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      curly: 'error',
+      eqeqeq: ['error', 'always'],
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-var': 'error',
+      'object-shorthand': 'warn',
+      'prefer-const': 'warn',
+    },
+  },
+];
