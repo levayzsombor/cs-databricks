5702b52695aa67962790943e75132449f74a55f6
Zsombor Levay <levay1990@gmail.com>
2026-07-20 21:44:33 +0000

    fail fast

--- .github/workflows/scripts/common/failed-test.sh
+++ .github/workflows/scripts/common/failed-test.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 TEST_STEP_OUTCOME="${TEST_STEP_OUTCOME:-unknown}"
 
 if [[ "${TEST_STEP_OUTCOME}" != "success" ]]; then
-    echo "Test step failed earlier. Marking job as failed at the end."
-    exit 1
+  echo "Test step failed earlier. Marking job as failed at the end."
+  exit 1
 fi
 
 echo "Test step outcome is success."
